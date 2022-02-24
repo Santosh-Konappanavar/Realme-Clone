59b0c707c23910021c06a3525000d8ab148a1e49
Anowar Hossain <anowar3042@gmail.com>
2022-02-24 22:20:51 +0530

    navbar

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,138 @@
+import "../Style/Header.css";
+
+export const Header = () => {
+  return (
+    <div className="header_container">
+      <div className="mall-plus">
+        <div className="header-plus">
+          <ul>
+            <li>
+              <a href="" className="tag">
+                realmeow
+              </a>
+            </li>
+            <li>
+              <span>|</span>
+            </li>
+            <li>
+              <a href="" className="tag">
+                Brand
+              </a>
+            </li>
+            <li>
+              <span>|</span>
+            </li>
+            <li>
+              <a href="" className="tag">
+                Support
+              </a>
+            </li>
+            <li>
+              <span>|</span>
+            </li>
+            <li>
+              <a href="" className="tag">
+                Community
+              </a>
+            </li>
+            <li>
+              <span>|</span>
+            </li>
+            <li>
+              <a href="" className="tag">
+                App
+              </a>
+            </li>
+            <li>
+              <span>|</span>
+            </li>
+            <li>
+              <a href="" className="tag">
+                Bulk Order
+              </a>
+            </li>
+            <li>
+              <span>|</span>
+            </li>
+            <li>
+              <a href="" className="tag">
+                UI 3.0
+              </a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="" className="tag">
+                Login
+              </a>
+            </li>
+            <li>
+              <span>|</span>
+            </li>
+            <li>
+              <a href="" className="tag">
+                My Order
+              </a>
+            </li>
+            <li>
+              <span>|</span>
+            </li>
+            <li>
+              <a href="" className="tag">
+                Cart
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="wrapper">
+        <div className="nav">
+          <ul className="nav-menu">
+            <li className="false">
+              <div>
+                <a href="" className="tag">
+                  realme
+                </a>
+              </div>
+            </li>
+            <li className="false">
+              <a href="" className="tag">
+                realme Smartphones
+              </a>
+            </li>
+            <li className="false">
+              <a href="" className="tag">
+                narzo Smartphones
+              </a>
+            </li>
+            <li className="false">
+              <a href="" className="tag">
+                realme TV
+              </a>
+            </li>
+            <li className="false">
+              <a href="" className="tag">
+                Laptop & Tablet
+              </a>
+            </li>
+            <li className="false">
+              <a href="" className="tag">
+                Audio
+              </a>
+            </li>
+            <li className="false">
+              <a href="" className="tag">
+                Smart Life
+              </a>
+            </li>
+            <li className="false">
+              <a href="" className="tag">
+                Accessories & Lifestyle
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
