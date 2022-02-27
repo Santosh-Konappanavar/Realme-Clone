3d1b71d5289e1c5e161002f8d7edbdb63d0b7577
Anowar Hossain <anowar3042@gmail.com>
2022-02-27 16:03:47 +0530

    payment view done

---
+++ client/src/Components/Payment.jsx
@@ -0,0 +1,76 @@
+import "../Style/Payment.css";
+
+export const Payment = () => {
+  return (
+    <div className="container">
+      <h1>Make Your Payment Now</h1>
+      <div className="first-row">
+        <div className="owner">
+          <h3>Card Holder's Name</h3>
+          <div className="input-field">
+            <input id="name" type="text" placeholder="Enter Your Name Here" />
+          </div>
+        </div>
+        <div className="cvv">
+          <h3>CVV</h3>
+          <div className="input-field">
+            <input id="password" type="password" placeholder="Enter  Number" />
+          </div>
+          <p id="in_pass"></p>
+        </div>
+      </div>
+      <div className="second-row">
+        <div className="card-number">
+          <h3>Card Number</h3>
+          <div className="input-field">
+            <input
+              id="card_number"
+              type="text"
+              placeholder="Enter Your Card Number Here"
+            />
+          </div>
+          <p id="in_nm"></p>
+        </div>
+      </div>
+      <div className="third-row">
+        <h3>Valid Upto</h3>
+        <div className="selection">
+          <div className="date">
+            <select name="months" id="months">
+              <option value="Jan">Jan</option>
+              <option value="Feb">Feb</option>
+              <option value="Mar">Mar</option>
+              <option value="Apr">Apr</option>
+              <option value="May">May</option>
+              <option value="Jun">Jun</option>
+              <option value="Jul">Jul</option>
+              <option value="Aug">Aug</option>
+              <option value="Sep">Sep</option>
+              <option value="Oct">Oct</option>
+              <option value="Nov">Nov</option>
+              <option value="Dec">Dec</option>
+            </select>
+            <select name="years" id="years">
+              <option value="2025">2025</option>
+              <option value="2024">2024</option>
+              <option value="2023">2023</option>
+              <option value="2022">2022</option>
+              <option value="2021">2021</option>
+              <option value="2020">2020</option>
+              <option value="2019">2019</option>
+              <option value="2018">2018</option>
+              <option value="2017">2017</option>
+              <option value="2016">2016</option>
+              <option value="2015">2015</option>
+            </select>
+          </div>
+          <div className="cards">
+            <img src="https://utils.imimg.com/payments/pwim_files/pwim_modes_v5.png" />
+          </div>
+        </div>
+        <p id="exp"></p>
+      </div>
+      <a id="payment">Pay Now</a>
+    </div>
+  );
+};
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Footer } from "../Components/Footer";
 import { Header } from "../Components/Header";
+import { Payment } from "../Components/Payment";
 
 const Home = () => {
-  return <Footer />;
+  return (
+    <div>
+      <Payment />
+    </div>
+  );
 };
 
 export default Home;
